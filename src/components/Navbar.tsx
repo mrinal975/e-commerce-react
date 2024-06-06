e4df90e1d817ec4412d6593ea8d568e688c8193d
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-06-06 13:05:06 +0600

    navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { CiUser, CiShoppingCart } from "react-icons/ci";
 function Navbar() {
   return (
     <div className="top-0 sticky bg-white">
-      <div className="container">
+      <div className="container mt-4">
         <div className=" flex justify-between items-center">
           <h1 className="font-bold text-4xl">Shopping</h1>
           <div className="flex gap-4 md:gap-8 items-center">
@@ -15,15 +15,21 @@ function Navbar() {
                 <CiUser />
               </div>
               <div className="">
-                <a>Sign in</a>
+                <a href="#" className="text-gray-500">
+                  Sign in
+                </a>
               </div>
             </div>
-            <div className="">
+            <div className="text-gray-500 text-3xl relative cursor-pointer">
               <CiShoppingCart />
+              <div className="absolute -top-3 -right-2 bg-red-500 rounded-full text-white text-sm w-5 h-5 flex justify-center items-center">
+                0
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="border-b border-gray-200 pt-4"></div>
     </div>
   );
 }
